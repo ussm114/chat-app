77dc950d9674b27b0501bb877b93de561ffbd95d
ussm114 <ussm114@gmail.com>
2018-02-09 21:38:04 +0100

    create new user, and alert others

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ var io = socketIO(server);  // conf. server to user socket io
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'welcome!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'new user joined',
+    createdAt: new Date().getTime()
+  });
+
   console.log('new user connected');
   socket.on('createMessage', (message) => {
     console.log('message: ', message);
@@ -20,6 +32,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // });
   });
   socket.on('disconnect', () => {
     console.log('user disconnected');
