4cda1148ceaab6e7a9d2887a0cd10a5cf9ff13d5
ussm114 <ussm114@gmail.com>
2018-02-09 21:09:03 +0100

    emit new message on createMessage

--- server/server.js
+++ server/server.js
@@ -13,26 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
-  socket.emit('newMessage', {
-    from: 'as',
-    text: 'asdfadf',
-    createdAt: 1234
-  });
-
   socket.on('createMessage', (message) => {
     console.log('message: ', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
-
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-
-  // socket.on('createMessage', (message) => {
-  //   socket.emit('newMessage', {
-  //     text: 'response',
-  //     to: 'andrew'
-  //   });
-  // });
 });
 
 server.listen(port, () => {
